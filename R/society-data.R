bb83ab35bb86bccd4e7a353a6273a110048f7103
Joe Wheatley <joe.wheatley@ucd.ie>
2021-02-17 17:08:35 +0000

    Assortative social network builder for use with uptake models

---
+++ R/society-data.R
@@ -0,0 +1,14 @@
+#' Agent demographic variables.
+#'
+#' A dataset derives from a subset of 2018 survey variables for drivers or prospective drivers
+#'
+#' @format A data frame with 924 rows and 12 variables:
+#' \describe{
+#'   \item{ID}{agent ID 1-924}
+#'   \item{age}{5 age ranges  "25-34" "35-44" "45-54" "18-24" "55+" }
+#'   \item{class}{two classes }
+#'   \item{gender}{ }
+#'   ...
+#' }
+#' @source 2018 Survey
+"society"
